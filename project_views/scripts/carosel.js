df0383499fba60ea7ab528f4150882e32c87cf58
vpk79 <someone_bul@yahoo.com>
2024-03-19 16:55:52 +0200

    update

--- project_views/scripts/carosel.js
+++ project_views/scripts/carosel.js
@@ -1,40 +1,40 @@
-const multipleItemCarousel = document.querySelector('#myCarousel2');
+// const multipleItemCarousel = document.querySelector('#myCarousel2');
 
 
 
-// if(window.matchMedia("(min-width:576px").matches){
+// // if(window.matchMedia("(min-width:576px").matches){
     
-// } else 
+// // } else 
 
 
 
 
 
 
-    // const carousel = new bootstrap.Carousel(multipleItemCarousel, {
-    //     interval: false
-    // })
-var carouselWidth = $('#myCarousel2 .carousel-inner')[0].scrollWidth;
-var cardWidth = $('#myCarousel2 .carousel-item').width();
+//     // const carousel = new bootstrap.Carousel(multipleItemCarousel, {
+//     //     interval: false
+//     // })
+// var carouselWidth = $('#myCarousel2 .carousel-inner')[0].scrollWidth;
+// var cardWidth = $('#myCarousel2 .carousel-item').width();
 
 
-var scrollPosition = 0;
+// var scrollPosition = 0;
 
-$('#myCarousel2 .carousel-control-next').on('click', function () {
-    if (scrollPosition < (carouselWidth - (cardWidth * 5))) {
-        scrollPosition = scrollPosition + cardWidth;
-       console.log(scrollPosition);
-        $('#myCarousel2 .carousel-inner').animate({ scrollLeft: scrollPosition }, 600)
-    }
+// $('#myCarousel2 .carousel-control-next').on('click', function () {
+//     if (scrollPosition < (carouselWidth - (cardWidth * 5))) {
+//         scrollPosition = scrollPosition + cardWidth;
+//        console.log(scrollPosition);
+//         $('#myCarousel2 .carousel-inner').animate({ scrollLeft: scrollPosition }, 600)
+//     }
 
-});
+// });
 
-$('#myCarousel2 .carousel-control-prev').on('click', function () {
-    if (scrollPosition > 0) {
+// $('#myCarousel2 .carousel-control-prev').on('click', function () {
+//     if (scrollPosition > 0) {
         
-        scrollPosition = scrollPosition - cardWidth;
-        console.log(scrollPosition);
-        $('#myCarousel2 .carousel-inner').animate({ scrollLeft: scrollPosition }, 600)
-    }
+//         scrollPosition = scrollPosition - cardWidth;
+//         console.log(scrollPosition);
+//         $('#myCarousel2 .carousel-inner').animate({ scrollLeft: scrollPosition }, 600)
+//     }
 
-});
+// });
