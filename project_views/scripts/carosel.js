6f519d47f051702d8793cf3f44c96e85c5ccf62d
vpk79 <someone_bul@yahoo.com>
2024-03-19 16:16:47 +0200

    update

--- project_views/scripts/carosel.js
+++ project_views/scripts/carosel.js
@@ -1,11 +1,40 @@
-var caroselWidth = $('#myCarousel2 .carousel-inner')[0].scrollWidth;
+const multipleItemCarousel = document.querySelector('#myCarousel2');
+
+
+
+// if(window.matchMedia("(min-width:576px").matches){
+    
+// } else 
+
+
+
+
+
+
+    // const carousel = new bootstrap.Carousel(multipleItemCarousel, {
+    //     interval: false
+    // })
+var carouselWidth = $('#myCarousel2 .carousel-inner')[0].scrollWidth;
 var cardWidth = $('#myCarousel2 .carousel-item').width();
 
 
 var scrollPosition = 0;
 
-$('#myCarousel2 .carousel-control-next').on('click', function(){
-    scrollPosition = scrollPosition + cardWidth;
-    $('#myCarousel2 .carousel-inner').animate({scrollLeft: scrollPosition}, 600)
+$('#myCarousel2 .carousel-control-next').on('click', function () {
+    if (scrollPosition < (carouselWidth - (cardWidth * 5))) {
+        scrollPosition = scrollPosition + cardWidth;
+       console.log(scrollPosition);
+        $('#myCarousel2 .carousel-inner').animate({ scrollLeft: scrollPosition }, 600)
+    }
+
 });
 
+$('#myCarousel2 .carousel-control-prev').on('click', function () {
+    if (scrollPosition > 0) {
+        
+        scrollPosition = scrollPosition - cardWidth;
+        console.log(scrollPosition);
+        $('#myCarousel2 .carousel-inner').animate({ scrollLeft: scrollPosition }, 600)
+    }
+
+});
