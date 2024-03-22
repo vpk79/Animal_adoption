3e4c4fcd064331351fe869fca6f750aa99940a12
vpk79 <someone_bul@yahoo.com>
2024-03-22 22:04:48 +0200

    up

---
+++ project_views/scripts/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyA8SgSbLZRZLDGJGXsQoCj6t0Z-yq4YgbM",
+    authDomain: "animaladoption-95397.firebaseapp.com",
+    projectId: "animaladoption-95397",
+    storageBucket: "animaladoption-95397.appspot.com",
+    messagingSenderId: "1080972947602",
+    appId: "1:1080972947602:web:3e1d677ce8a0b69805b64b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
