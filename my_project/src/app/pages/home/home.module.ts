0e5e27c2914f6a03c4c8adf2d4f880c2a2e4a409
vpk79 <someone_bul@yahoo.com>
2024-03-24 20:42:17 +0200

    up

--- my_project/src/app/pages/home/home.module.ts
+++ my_project/src/app/pages/home/home.module.ts
@@ -1,17 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
+import { HeaderComponent } from './header/header.component';
+import { HeaderCardsComponent } from './header-cards/header-cards.component';
 
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    HeaderComponent,
+    HeaderCardsComponent
   ],
   imports: [
     CommonModule
   ],
-  exports: [HomeComponent]
+  exports: [HomeComponent, HeaderComponent]
 
 })
 export class HomeModule { }
