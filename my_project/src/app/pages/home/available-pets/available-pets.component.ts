19f77da48f06ccf2d8b4b2c26939677289c2a086
vpk79 <someone_bul@yahoo.com>
2024-03-29 20:58:07 +0200

    up

--- my_project/src/app/pages/home/available-pets/available-pets.component.ts
+++ my_project/src/app/pages/home/available-pets/available-pets.component.ts
@@ -11,17 +11,23 @@ export class AvailablePetsComponent implements OnInit{
 
   constructor(private service: Service){ }
 
-  animalsData: { [key: string]: any }[] = [];
+  animalsData: Animals[] = [];
+  animalsDataArray: any[][] = [];
   
 
   ngOnInit(): void {
 
-    this.service.getAnimalsDataByStatus('Reserved').subscribe({
+    this.service.getAnimalsDataByStatus('Available').subscribe({   // Could be 'Reserved' or 'Available'
       next: (data:any) => {
-       this.animalsData = data;
-      //  console.log(this.animalsData);
+      //  this.animalsData = data;
+        for (let i = 0; i < data.length; i += 4) {
+          this.animalsDataArray.push(data.slice(i, i + 4));
+        }
+       console.log(this.animalsDataArray);
       }
     });
+
+
 
     
     
